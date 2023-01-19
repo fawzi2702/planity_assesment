ab333d4abcb9761dfb142ce356a347b5e4b72f1a
fawzi <fawzi@belorder.com>
2023-01-19 02:17:42 +0100

    variabilize calendar start and end time in environment

--- src/core/types/environment.d.ts
+++ src/core/types/environment.d.ts
@@ -2,5 +2,7 @@
 namespace NodeJS {
   interface ProcessEnv extends NodeJS.ProcessEnv {
     readonly REACT_APP_CALENDAR_EVENT_ENDPOINT: string
+    readonly REACT_APP_CALENDAR_DAY_START: number
+    readonly REACT_APP_CALENDAR_DAY_END: number
   }
 }
--- src/core/utils/time.ts
+++ src/core/utils/time.ts
@@ -1,7 +1,7 @@
 import moment from 'moment'
 
-export const CALENDAR_DAY_START = 540 // in minutes
-export const CALENDAR_DAY_END = 1260 // in minutes
+export const CALENDAR_DAY_START = process.env.REACT_APP_CALENDAR_DAY_START // in minutes
+export const CALENDAR_DAY_END = process.env.REACT_APP_CALENDAR_DAY_END // in minutes
 export const CALENDAR_DAY_DURATION = CALENDAR_DAY_END - CALENDAR_DAY_START // in minutes
 
 export const parseTime = (time: string): number => {
